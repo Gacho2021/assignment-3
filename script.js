755ab86615ea0a31e533c7bde82d0434661bd751
Gacho2021 <87105994+Gacho2021@users.noreply.github.com>
2022-01-26 21:26:37 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+//set varibles for the number and activity
+let userName = prompt("Lets play the sum game.\n Please enter your name");
+let numberOne = parseInt(prompt("Enter first number"));
+let numberTwo = parseInt(prompt("Enter second number"));
 
+let total = numberOne + numberTwo
+let activity = `Hi, ${userName}! The sum of ${numberOne} + ${numberTwo} is ${total}`
+
+//set variables for selectors
+let firstP = document.getElementById('firstP')
+
+//inserted variable into the objects
+firstP.innerHTML = activity
